9332553113fd7d80a2a396235375e5b7b59eba25
Podologia-Martita <chelomiranda64@gmail.com>
2025-09-30 15:51:17 -0300

    Actualizar supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Se leen las variables de entorno definidas en Vercel o .env
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-// Crear cliente Supabase
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
